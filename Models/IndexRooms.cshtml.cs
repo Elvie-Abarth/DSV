7686750a66e547292244cd897f06de619d181744
Esti Brussé <esb002@edu.zealand.dk>
2025-04-04 14:04:10 +0200

    Made an Model folder add the INdexRooms.cshtml(.cs) in there.

---
+++ Models/IndexRooms.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DSV_Book_a_room.Models
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
