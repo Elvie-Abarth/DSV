3a4b61c3034e0815e9fdc48618a9342adcae6dc8
Esti Brussé <esb002@edu.zealand.dk>
2025-04-05 16:37:50 +0200

    Added bookingviewmodel

---
+++ Models/BookingViewModel.cs
@@ -0,0 +1,9 @@
+public class BookingViewModel
+{
+    public int BookingId { get; set; }
+    public int RoomId { get; set; }
+    public string RoomName { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public string Description { get; set; }
+}
