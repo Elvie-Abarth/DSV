d0d45513373f1f2695ddd3e86dca40aa6e94c4d0
Esti Brussé <esb002@edu.zealand.dk>
2025-04-05 16:37:19 +0200

    Added

---
+++ Models/Booking.cs
@@ -0,0 +1,18 @@
+﻿namespace DSV_Book_a_room.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Description { get; set; }
+        public int RoomId { get; set; }
+        public Booking(DateTime startTime, DateTime endTime, int roomId, string description)
+        {
+            StartTime = startTime;
+            EndTime = endTime;
+            RoomId = roomId;
+            Description = description;
+        }
+    }
+}
