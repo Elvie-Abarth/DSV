cfe0635f5dbc285bd4e4831c891df570d645bd54
Esti Brussé <esb002@edu.zealand.dk>
2025-04-05 16:38:30 +0200

    Moddefite DsvDbContext

--- Models/DsvDbContext.cs
+++ Models/DsvDbContext.cs
@@ -5,14 +5,12 @@ namespace DSV_Book_a_room.Models
     public class DsvDbContext : DbContext
     {
         public DbSet<Room> Rooms { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-
-
             optionsBuilder.UseInMemoryDatabase("DSV-Db");
-
-
         }
     }
 }
